9a286d066198f49349cacad27e2937badd4dcda0
Pavel Vyaznikov <sha.cehca@yandex.ru>
2023-12-06 19:00:54 +0300

    mic script fix

--- mic_stream.py
+++ mic_stream.py
@@ -1,12 +1,13 @@
 import argparse
 from datetime import datetime, timedelta, time as date_time
 import os
-import time
 from collections import defaultdict
 import wave
+from queue import Queue
 
 import torch
-import pyaudio
+from torchaudio.transforms import Resample
+import sounddevice as sd
 import numpy as np
 import yaml
 from munch import munchify
@@ -23,18 +24,26 @@ class StreamPrediction:
         self.model = torch.jit.load(self.cfg.ckpt_path)
 
         self.logger = defaultdict(list)
+        self.queue = Queue()
+
+        self.default_mic_sr = int(
+            sd.query_devices(self.cfg.default_mic_name)["default_samplerate"]
+        )
+        self.transform = Resample(
+            orig_freq=self.default_mic_sr,
+            new_freq=self.cfg.sample_rate,
+        )
+    
+    def callback(self, indata, outdata, frames, time, status):
+        if status:
+            print(status)
+
+        audio = torch.t(torch.tensor(indata))
+        audio = self.transform(audio)
+        
+        self.queue.put(torch.unsqueeze(audio, 0))
 
     def start_stream(self):
-        p = pyaudio.PyAudio()
-
-        stream = p.open(
-            format=pyaudio.paFloat32,
-            channels=1,
-            rate=self.cfg.sample_rate,
-            input=True,
-            frames_per_buffer=self.chunk_samples,
-        )
-
         os.makedirs(self.cfg.recorded_audio_path, exist_ok=True)
         os.makedirs(self.cfg.label_path, exist_ok=True)
 
@@ -54,44 +63,42 @@ class StreamPrediction:
             f"mic_label_{curr_time}.xlsx",
         )
 
-        stream.start_stream()
-
         try:
-            while True:
-                data = np.frombuffer(stream.read(self.chunk_samples), dtype="float32")
-                data = np.expand_dims(data, 0)
-                data = np.expand_dims(data, 0)
+            with sd.Stream(
+                device=self.cfg.default_mic_name,
+                blocksize=self.default_mic_sr,
+                dtype="float32",
+                channels=1,
+                callback=self.callback,
+            ):
+                while True:
+                    data = self.queue.get()
 
-                with torch.no_grad():
-                    pred = self.model(torch.tensor(data))
+                    with torch.no_grad():
+                        pred = self.model(data)
 
-                record_wav.writeframes(
-                    (np.squeeze(data, axis=0) * (2**15 - 1)).astype("<h")
-                )
-                record_time += timedelta(seconds=1)
+                    record_wav.writeframes(
+                        (np.squeeze(data.numpy(), axis=0) * (2**15 - 1)).astype("<h")
+                    )
+                    record_time += timedelta(seconds=1)
 
-                self.logger["Real Time"].append(str(datetime.now()))
-                self.logger["Audio Time"].append(str(record_time))
-                [
-                    self.logger[f"Class {idx}"].append(el.item())
-                    for idx, el in enumerate(torch.softmax(pred.squeeze(), -1))
-                ]
+                    self.logger["Real Time"].append(str(datetime.now()))
+                    self.logger["Audio Time"].append(str(record_time))
+                    [
+                        self.logger[f"Class {idx}"].append(el.item())
+                        for idx, el in enumerate(torch.softmax(pred.squeeze(), -1))
+                    ]
 
-                log_str = f"{datetime.now().strftime('%H:%M:%S:%f')} | {torch.softmax(pred, -1)};"
+                    log_str = f"{datetime.now().strftime('%H:%M:%S:%f')} | {torch.softmax(pred, -1)};"
 
-                print(
-                    log_str,
-                    sep="",
-                    end="\n",
-                    flush=True,
-                )
-
-                time.sleep(self.cfg.chunk_size / 1000)
-
+                    print(
+                        log_str,
+                        sep="",
+                        end="\n",
+                        flush=True,
+                    )
+                    
         except (KeyboardInterrupt, SystemExit):
-            stream.stop_stream()
-            stream.close()
-
             d = pd.DataFrame(self.logger)
             d.to_excel(label_file_name, index=False)
 
